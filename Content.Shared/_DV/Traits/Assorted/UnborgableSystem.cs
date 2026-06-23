36370b01fdf336474a71b70214b3ba160e692791
Pachat-Kas <pachat.kas@gmail.com>
2026-06-23 18:12:39 +0200

    fixes

--- Content.Shared/_DV/Traits/Assorted/UnborgableSystem.cs
+++ Content.Shared/_DV/Traits/Assorted/UnborgableSystem.cs
@@ -1,18 +1,16 @@
-using Content.Shared.Body.Components;
-using Content.Shared.Body.Organ;
-using Content.Shared.Body.Systems;
+using Content.Shared.Body;
 using Content.Shared.Examine;
 using Content.Shared.Movement.Components; // TODO: use BrainComponent instead of InputMover when shitmed is merged
-using Robust.Shared.Utility;
+
 
 namespace Content.Shared._DV.Traits.Assorted;
 
 /// <summary>
 /// Adds a warning examine message to brains with <see cref="UnborgableComponent"/>.
 /// </summary>
-public sealed class UnborgableSystem : EntitySystem
+public sealed partial class UnborgableSystem : EntitySystem
 {
-    [Dependency] private readonly SharedBodySystem _body = default!;
+    [Dependency] private BodySystem _body = default!;
 
     public override void Initialize()
     {
@@ -28,7 +26,8 @@ public sealed class UnborgableSystem : EntitySystem
     public bool IsUnborgable(Entity<BodyComponent?> ent)
     {
         // technically this will apply for any organ not just brain, but assume nobody will be evil and do that
-        return _body.GetBodyOrganEntityComps<UnborgableComponent>(ent).Count > 0;
+        _body.TryGetOrgansWithComponent<UnborgableComponent>(ent, out var organs);
+        return organs.Count > 0;
     }
 
     private void OnMapInit(Entity<UnborgableComponent> ent, ref MapInitEvent args)
@@ -36,7 +35,7 @@ public sealed class UnborgableSystem : EntitySystem
         if (!TryComp<BodyComponent>(ent, out var body))
             return;
 
-        var brains = _body.GetBodyOrganEntityComps<InputMoverComponent>((ent.Owner, body));
+        _body.TryGetOrgansWithComponent<InputMoverComponent>((ent.Owner, body), out var brains);
         foreach (var brain in brains)
         {
             EnsureComp<UnborgableComponent>(brain);
