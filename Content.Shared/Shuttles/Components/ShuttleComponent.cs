82746b81e562ae8a935d2f70666d26a96d5f07f2
Pachat-Kas <pachat.kas@gmail.com>
2026-09-19 00:11:14 +0200

    wag

--- Content.Shared/Shuttles/Components/ShuttleComponent.cs
+++ Content.Shared/Shuttles/Components/ShuttleComponent.cs
@@ -14,7 +14,6 @@ public sealed partial class ShuttleComponent : Component
     /// <summary>
     /// Thrust gets multiplied by this value if it's for braking.
     /// </summary>
-    [DataField]
     public const float BrakeCoefficient = 1.5f;
 
     /// <summary>
@@ -22,8 +21,7 @@ public sealed partial class ShuttleComponent : Component
     /// </summary>
     [DataField]
     public float BaseMaxLinearVelocity = 60f;
-
-    [DataField]
+    
     public const float MaxAngularVelocity = 4f;
 
     /// <summary>
